fac641bbb6db366a6514089fadfc357c3970444d
RINGOPAI <ringo.capsule@gmail.com>
2017-09-16 17:24:56 +0900

    updateアクションの設定とpry-railsのインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    user = User.find(current_user)
+    user.update(create_params)
+    redirect_to controller: :messages, action: :index
+  end
+
+private
+def create_params
+  params.require(:user).permit(:name, :email)
+end
+
 end
