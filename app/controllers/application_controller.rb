445751f15af562ff05ce5b6b0cfd60d47322fe64
RINGOPAI <ringo.capsule@gmail.com>
2017-09-16 12:48:50 +0900

    漏れていたサインアップ機能の実装
    -ユーザー登録時にnameキーの追加を許可
    -user.scssを適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+# ユーザー登録時にパラメーターのname属性の受け取りを許可
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
