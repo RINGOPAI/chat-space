635ab1921f637f78b151afc506da325bacf40c34
RINGOPAI <ringo.capsule@gmail.com>
2017-09-20 17:31:03 +0900

    グループ作成機能の実装2
    ・Group.createの記述
    ・ビューの記述の修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,19 @@ class GroupsController < ApplicationController
   end
 
   def create
+    Group.create(create_params)
+    redirect_to root_path, notice: "グループを作成しました"
   end
 
   def edit
   end
 
   def update
+    redirect_to root_path
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
