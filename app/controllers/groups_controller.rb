555fc2b94df15cd268c3e4f907366bf848525628
RINGOPAI <ringo.capsule@gmail.com>
2017-09-22 01:39:38 +0900

    ルートパスの変更
    ・rootをmessages#indexからgroups#indexに
    ・views/messagesファイルをすべてviews/groupsに移動
    ・groupsコントローラーにindexアクションを定義
    ・messagesコントローラーの削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,10 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @groups = current_user.groups
+  end
+
   def new
     @group = Group.new
   end
