3f130979b85af6a16a8dbc506955ef734d129f6d
RINGOPAI <ringo.capsule@gmail.com>
2017-09-21 18:46:19 +0900

    ログインuserのgroupの取得の仕方を変更して1行に

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,16 +2,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    user_groups
-  end
-
-  def user_groups
-    members = Member.where(user_id: current_user.id)
-    @groups = []
-    members.each do |member|
-      @groups << Group.find(member.group_id)
-    end
-    return @groups
+    @groups = User.find(current_user.id).groups
   end
 
 end
