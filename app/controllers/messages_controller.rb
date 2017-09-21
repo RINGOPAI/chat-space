dad9a50ab3c461df86a656bc52ce450c7f12e163
RINGOPAI <ringo.capsule@gmail.com>
2017-09-21 17:18:00 +0900

    サイドバーにグループを表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,11 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    members = Member.where(user_id: current_user.id)
+    @groups = []
+    members.each do |member|
+      @groups << Group.find(member.group_id)
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :edit, :create, :update]
+  resources :groups
   root 'messages#index'
 
 end
