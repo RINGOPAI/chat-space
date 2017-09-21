2ced657b9b6db9c73c41716468b1071cce00f3e7
RINGOPAI <ringo.capsule@gmail.com>
2017-09-21 18:00:05 +0900

    user_groupsメソッドを新しく定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,16 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    user_groups
+  end
+
+  def user_groups
     members = Member.where(user_id: current_user.id)
     @groups = []
     members.each do |member|
       @groups << Group.find(member.group_id)
     end
+    return @groups
   end
 
 end
