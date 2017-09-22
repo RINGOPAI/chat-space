39c135d52af390faf7e7a1193c056d3494eb5282
RINGOPAI <ringo.capsule@gmail.com>
2017-09-22 12:17:25 +0900

    private下に記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,12 +30,12 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit_group
-    @group = Group.find(params[:id])
-  end
-
   private
   def create_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def edit_group
+    @group = Group.find(params[:id])
   end
 end
