0b377b6dfdaed04369cabe77eafd1ed8d3e62a64
RINGOPAI <ringo.capsule@gmail.com>
2017-09-25 16:12:08 +0900

    サイドバーにそのグループの最新のメッセージを表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   has_many :messages
+
+  def sidebar_message
+    if self.messages.last
+      if self.messages.last.body?
+        self.messages.last.body
+      elsif self.messages.last.image?
+        "画像が投稿されています"
+      end
+    else
+      "まだメッセージがありません"
+    end
+  end
 end
