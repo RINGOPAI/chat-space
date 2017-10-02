d9751fd4f68c15965a83a95cdf655a9a82af232f
RINGOPAI <ringo.capsule@gmail.com>
2017-10-02 16:43:46 +0900

    テスト1

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
-describe MessageController, type: :controller do
-  let(:user)  { create(:user) }
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
 
   describe "GET #index" do
     before do
       login_user user
     end
-    it "renders the :index template" do
+
+    it "メッセージアクションで定義しているインスタンス変数はあるか" do
+      @group = user.groups.first
+      binding.pry
+      get :index, params: { group_id: @group.id }
+      groups = user.groups
+      expect(assigns(:groups)).to eq groups
+    end
+    it "メッセージ一覧のビューが表示されているか" do
+      @group = user.groups
+      get :index, params: { group_id: @group.id }
+      groups = user.groups
+      expect(response).to render_template :index
     end
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,9 @@ FactoryGirl.define do
     pass = Faker::Number.number(8)
     password              pass
     password_confirmation pass
+
+    after(:create) do |user|
+      create(:member, user: user, group: create(:group))
+    end
   end
 end
