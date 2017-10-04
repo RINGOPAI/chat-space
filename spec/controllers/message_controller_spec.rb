4d4e73d28e64472666e85e51fdceeccd7683b04f
RINGOPAI <ringo.capsule@gmail.com>
2017-10-04 15:39:30 +0900

    テストの実施

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -41,6 +41,26 @@ describe MessagesController do
   end
 
   describe "POST #create" do
+    context "ログインしている場合" do
+      before do
+        login_user user
+      end
+
+      it "保存に成功" do
+        expect{post :create, params: { group_id: group, message: attributes_for(:message, user: user, group: group) }}.to change(Message, :count).by(1)
+      end
+
+      it "保存に失敗" do
+        expect{post :create, params: { group_id: group, message: attributes_for(:message, body: nil, image: nil, user: user, group: group) }}.not_to change(Message, :count)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "ログインページにリダイレクトされるか" do
+        post :create, params: { group_id: group, message: attributes_for(:message, user: user, group: group) }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
 
 end
