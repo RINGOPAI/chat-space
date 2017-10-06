f35356c090f6eac928ee18b4ac7e3f681b59976b
RINGOPAI <ringo.capsule@gmail.com>
2017-10-06 20:33:58 +0900

    受け取ったJSONでHTMLの組み立て

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,4 +1,35 @@
 $(function() {
+
+function appendNewMessage(message) {
+  var html = `<div class = "group__message--name">
+                <%= message.user.name %>
+              </div>
+              <dic class = "group__message--time">
+                <%= message.created_at %>
+              </div>
+              <div class = "group__message--text">
+                <%= message.body %>
+              </div>
+              <div class = "group__message--image">
+                <% if message.image? %>
+                  <%= image_tag(message.image.url) %>
+              </div>`
+//   .group__message--name
+//     = message.user.name
+//   .group__message--time
+//     = message.created_at
+//   .group__message--text
+//     = message.body
+//   .group__message--image
+//     - if message.image?
+//       = image_tag message.image.url
+//
+//         <image src= "#{message.image.url}" >
+
+  $(".group__message").append(html);
+}
+
+
   $(".group__form").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +41,15 @@ $(function() {
       url: url
       data: 'formData'
       dataType: 'json'
+    })
+
+    .done(function(message) {
+      // 個別のメッセージと同じHTML
+      appendNewMessage(message);
+
+    })
+    .fail(function(message) {
+
     })
 
   });
