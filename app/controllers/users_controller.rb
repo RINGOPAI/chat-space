b0dcf287205539fe4e6155375b60bf7e275f3614
RINGOPAI <ringo.capsule@gmail.com>
2017-10-11 16:16:40 +0900

    せっかく書いたjson.builderを使わない書き方になってしまっていたので修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    render json: @users
+    respond_to do |format|
+      format.json { }
+    end
   end
 
   def edit
